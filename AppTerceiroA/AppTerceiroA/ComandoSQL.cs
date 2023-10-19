0187cb01186e11ce66ccbf4c8b6dced9b0b24fc2
Lucas Gomes Moreira <lucas.moreira@colegiomodulo.com.br>
2023-10-19 13:15:54 -0300

    19/10/2023

--- AppTerceiroA/AppTerceiroA/ComandoSQL.cs
+++ AppTerceiroA/AppTerceiroA/ComandoSQL.cs
@@ -6,6 +6,6 @@ namespace AppTerceiroA
 {
     public static class ComandoSQL
     {
-        public static string insereNome = "INSERT INTO tbl_cliente(nome) VALUES (@nome)";
+        public static string insereNome = "INSERT INTO tbl_cliente(id_cliente, nome, sexo) VALUES ('1','Lusca','M')";
     }
 }
